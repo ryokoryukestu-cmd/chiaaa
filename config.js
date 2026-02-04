2d7506a41dfdbf4c0d42cc08c9d2cd9e02018988
ryokoryukestu-cmd <ryokoryukestu@gmail.com>
2026-02-04 12:00:25 -0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dngfn23by/video/upload/v1770235156/%E5%85%AC%E5%BC%8F%E3%81%BE%E3%81%97%E3%82%99%E3%81%8B%E3%82%8B%E3%81%A1%E3%81%84%E3%81%8B%E3%82%8F%E3%82%B9%E3%83%98%E3%82%9A%E3%82%B7%E3%83%A3%E3%83%AB%E3%83%9F%E3%83%A5%E3%83%BC%E3%82%B7%E3%82%99%E3%83%83%E3%82%AF%E3%83%92%E3%82%99%E3%83%86%E3%82%99%E3%82%AA%E3%83%9E%E3%82%B7%E3%82%99%E3%82%AB%E3%83%AB%E3%82%B7%E3%83%B3%E3%83%88%E3%82%99%E3%83%AD%E3%83%BC%E3%83%A0_qeteet.mp3
+        musicUrl: "https://res.cloudinary.com/dngfn23by/video/upload/v1770235156/%E5%85%AC%E5%BC%8F%E3%81%BE%E3%81%97%E3%82%99%E3%81%8B%E3%82%8B%E3%81%A1%E3%81%84%E3%81%8B%E3%82%8F%E3%82%B9%E3%83%98%E3%82%9A%E3%82%B7%E3%83%A3%E3%83%AB%E3%83%9F%E3%83%A5%E3%83%BC%E3%82%B7%E3%82%99%E3%83%83%E3%82%AF%E3%83%92%E3%82%99%E3%83%86%E3%82%99%E3%82%AA%E3%83%9E%E3%82%B7%E3%82%99%E3%82%AB%E3%83%AB%E3%82%B7%E3%83%B3%E3%83%88%E3%82%99%E3%83%AD%E3%83%BC%E3%83%A0_qeteet.mp3"
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
