2f7541e9f503b1401c741c59b072dcd9ab980b04
ryokoryukestu-cmd <ryokoryukestu@gmail.com>
2026-02-04 12:44:38 -0800

    Update config.js

--- config.js
+++ config.js
@@ -22,9 +22,9 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
+            text: "Do you luv me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
-            noBtn: "No",                                               // Text for "No" button
+            noBtn: "No...banned",                                               // Text for "No" button
             secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
         },
         second: {
