71cf066bcd6a938a558bccba34dc751b7a91cf99
ryokoryukestu-cmd <ryokoryukestu@gmail.com>
2026-02-04 12:43:56 -0800

    can2

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Will You Be My Fah-lentine? 💞💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
